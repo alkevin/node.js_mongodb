2360e2a1f7fc231d19c399977a2ffae5b2c967e2
Bryce <bryce.tavares@gmail.com>
2019-12-06 09:22:40 +0100

    add lessonRoutes

---
+++ src/api/routes/lessonRoutes.js
@@ -0,0 +1,19 @@
+module.exports = function(app){
+    const lesson = require('../controllers/lessonController');
+
+    // Routes
+
+    app.route('/lessons')
+    .get(lesson.list_all_lessons)
+    // .post(lesson.create_lesson);
+
+    // app.route('/lessons/test/:id')
+    // .put(lesson.findOneUpdate_lesson);
+    
+    
+    // app.route('/lessons/:id')
+    // .get(lesson.get_lesson)
+    // .put(lesson.update_lesson)
+    // .delete(lesson.delete_lesson);
+    
+}
