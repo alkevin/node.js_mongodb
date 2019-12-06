5d089a59273a004f1ae22b62c812de29bbd21033
LoryLeticee <loryleticee@gmail.com>
2019-12-06 12:02:51 +0100

    add more route for speakers

--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -29,10 +29,4 @@ module.exports = function(app){
     .get(user.get_user)
     .put(user.update_user)
     .delete(user.delete_user);
-
-
-    app.route('/speakers')
-    .get(speaker.list_all_speakers)
-    .post(speaker.create_speaker);
-    
 }
